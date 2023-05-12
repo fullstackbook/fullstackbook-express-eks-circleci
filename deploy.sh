cf4e44f23a9d52272098b870577aec3e91384646
Travis Luong <travis@travisluong.com>
2023-05-12 16:14:26 -0700

    update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 LATEST_SHA=$(git rev-parse HEAD)
-docker build -t express-eks:$LATEST_SHA
+docker build -t express-eks:$LATEST_SHA .
 docker tag express-eks:$LATEST_SHA 207100238581.dkr.ecr.us-east-2.amazonaws.com/express-eks:$LATEST_SHA
 docker push 207100238581.dkr.ecr.us-east-2.amazonaws.com/express-eks:$LATEST_SHA
-kubectl set image deployment/express-eks myapp=207100238581.dkr.ecr.us-east-2.amazonaws.com/express-eks:$LATEST_SHA -n default
+kubectl set image deployment/myapp myapp=207100238581.dkr.ecr.us-east-2.amazonaws.com/express-eks:$LATEST_SHA -n default
